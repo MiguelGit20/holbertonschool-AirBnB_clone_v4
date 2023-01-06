f46200137e717338ec7d8404e10c0b1bf3a3af5f
MiguelGit20 <miguel.torres.20@hotmail.com>
2023-01-06 15:43:34 -0500

    Filter places by Amenity

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,8 @@
+// JavaScript Script
 document.addEventListener('DOMContentLoaded', async () => {
+
+  // Function that makes a POST request to the API to retrieve the places and add them to the page.
+
   const response = await fetch('http://127.0.0.1:5001/api/v1/places_search/', {
     method: 'POST',
     headers: {
@@ -20,12 +24,34 @@ document.addEventListener('DOMContentLoaded', async () => {
           <div class="max_guest">${i.max_guest} Guest${i.max_guest > 1 ? 's' : ''}</div>
           <div class="number_rooms">${i.number_rooms} Bedroom${i.number_rooms > 1 ? 's' : ''}</div>
           <div class="number_bathrooms">${i.number_bathrooms} Bathroom${i.number_bathrooms > 1 ? 's' : ''} </div>
-        </div>
-        <div class="description">${i.description}</div>
-      `;
+          </div>
+          <div class="description">${i.description}</div>
+          `;
       document.querySelector('.places').appendChild(newArticle);
     }
+
   });
+
+});
+
+document.addEventListener('DOMContentLoaded', async () => {
+  // Function that adds functionality to the search button in filter Section.
+
+  const button = document.querySelector('button')
+  button.addEventListener('click', function () {
+    fetch('http://127.0.0.1:5001/api/v1/places_search/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "amenities": ["017ec502-e84a-4a0f-92d6-d97e27bb6bdf"] })
+    })
+      .then(response => response.json())
+      .then(response => console.log(JSON.stringify(response)))
+  })
+
+  // Function that makes a request to the API and checks the status.
 
   const apiClass = document.querySelector('.api_status');
   fetch('http://127.0.0.1:5001/api/v1/status/')
@@ -37,12 +63,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         apiClass.classList.remove('available');
       }
     });
-
-  const button = document.querySelector('button')
-  button.addEventListener('click', function () {
-    
-  })
 });
+
+// JQuery for veryfing and displaying amenities in the filter Section.
 
 $(function () {
   const idList = [];
