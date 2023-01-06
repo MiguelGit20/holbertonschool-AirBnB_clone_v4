dec7cfabccf244d145a9a54caba79e8b76a07025
abimont <abimontes00@gmail.com>
2023-01-05 22:13:48 -0500

    Update task 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const response = await fetch("http://127.0.0.1:5001/api/v1/places_search/", {
+  const response = await fetch('http://127.0.0.1:5001/api/v1/places_search/', {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
-    body: `{}`,
+    body: '{}'
   });
 
   response.json().then(data => {
-
     for (const i of data) {
-      console.log(i.name)
-      const newArticle = document.createElement("article");
+      const newArticle = document.createElement('article');
       newArticle.innerHTML = `
         <div class="title_box">
           <h2>${i.name}</h2>
@@ -24,25 +22,10 @@ document.addEventListener('DOMContentLoaded', async () => {
           <div class="number_bathrooms">${i.number_bathrooms} Bathroom${i.number_bathrooms > 1 ? 's' : ''} </div>
         </div>
         <div class="description">${i.description}</div>
-    `
-    document.querySelector(".places").appendChild(newArticle);
-     
-      // const newArticle = document.createElement("article");
-      // const divTitleBox = document.createElement("div")
-      // divTitleBox.classList.add("title_box")
-
-      // const divh2 = document.createElement("h2")
-      // divh2.textContent = i.name
-      // const divprice = document.createElement("div")
-      // divprice.classList.add("price_by_night")
-      // divprice.textContent = "$" + i.price_by_night
-
-      // divTitleBox.appendChild(divh2)
-      // divTitleBox.appendChild(divprice)
-      // newArticle.appendChild(divTitleBox)
-
-      // document.querySelector(".places").appendChild(newArticle);
-    }});
+      `;
+      document.querySelector('.places').appendChild(newArticle);
+    }
+  });
 
   const apiClass = document.querySelector('.api_status');
   fetch('http://127.0.0.1:5001/api/v1/status/')
@@ -59,7 +42,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 $(function () {
   const idList = [];
   const namesList = [];
-
 
   $('input[data-id]').change(function () {
     const name = $(this).attr('data-name');
@@ -79,4 +61,3 @@ $(function () {
     $('.amenities h4').text(namesList.join(', '));
   });
 });
-
